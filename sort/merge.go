642e24d18d28529af7185bf9db24e68baa145970
浜田　俊祐 <hamashun0820@icloud.com>
2023-12-20 14:15:38 +0900

    add: Add MergeSort implementation to sort package

---
+++ sort/merge.go
@@ -0,0 +1,50 @@
+package sort
+
+func MergeSort(arr []int) {
+	mergeSort(arr, 0, len(arr)-1)
+}
+
+func merge(arr []int, left, mid, right int) {
+	tmp := make([]int, right-left+1)
+	i := left
+	j := mid + 1
+	k := 0
+
+	for i <= mid && j <= right {
+		if arr[i] <= arr[j] {
+			tmp[k] = arr[i]
+			i++
+		} else {
+			tmp[k] = arr[j]
+			j++
+		}
+		k++
+	}
+
+	for i <= mid {
+		tmp[k] = arr[i]
+		i++
+		k++
+	}
+
+	for j <= right {
+		tmp[k] = arr[j]
+		j++
+		k++
+	}
+
+	for i := 0; i < k; i++ {
+		arr[left+i] = tmp[i]
+	}
+}
+
+func mergeSort(arr []int, left, right int) {
+	if left < right {
+		mid := (left + right) / 2
+
+		mergeSort(arr, left, mid)
+		mergeSort(arr, mid+1, right)
+
+		merge(arr, left, mid, right)
+	}
+}
