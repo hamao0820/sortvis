f884b118bf875110699895f6f2b24f52f657f404
浜田　俊祐 <hamashun0820@icloud.com>
2023-12-20 14:14:39 +0900

    test: Add additional test cases for BubbleSort function

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -18,6 +18,10 @@ func TestBubbleSort(t *testing.T) {
 		{"test1", args{[]int{3, 2, 1}}, []int{1, 2, 3}},
 		{"test2", args{[]int{3, 2, 1, 4, 5, 6, 7, 8, 9, 10}}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}},
 		{"test3", args{[]int{3, 7, 2, 8, 1, 6, 4, 0, -5, 6, 9}}, []int{-5, 0, 1, 2, 3, 4, 6, 6, 7, 8, 9}},
+		{"test4", args{[]int{2, 1}}, []int{1, 2}},
+		{"test5", args{[]int{1}}, []int{1}},
+		{"test6", args{[]int{1,1,1,1,1}}, []int{1,1,1,1,1}},
+		{"test7", args{[]int{1,1,1,1}}, []int{1,1,1,1}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
