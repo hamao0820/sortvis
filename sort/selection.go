17f4cf2e1e55a671b31fe98cd822576c588a480c
浜田　俊祐 <hamashun0820@icloud.com>
2023-12-21 14:25:11 +0900

    add: Add SelectionSortAsync function for asynchronous selection sort

--- sort/selection.go
+++ sort/selection.go
@@ -12,3 +12,17 @@ func SelectionSort(arr []int) {
 		swap(&arr[i], &arr[min])
 	}
 }
+
+func SelectionSortAsync(arr []int, c chan int) {
+	for i := 0; i < len(arr); i++ {
+		min := i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[min] {
+				min = j
+			}
+		}
+
+		<-c
+		swap(&arr[i], &arr[min])
+	}
+}
