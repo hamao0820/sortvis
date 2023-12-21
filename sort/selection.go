71620edab12bd537f19651f613acda9deef78490
浜田　俊祐 <hamashun0820@icloud.com>
2023-12-21 14:22:08 +0900

    add: Add SelectionSort function and test cases

---
+++ sort/selection.go
@@ -0,0 +1,14 @@
+package sort
+
+func SelectionSort(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		min := i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[min] {
+				min = j
+			}
+		}
+
+		swap(&arr[i], &arr[min])
+	}
+}
