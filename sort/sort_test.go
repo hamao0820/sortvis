a3bc367421889abc5a508f9e1a48095b064e124d
浜田　俊祐 <hamashun0820@icloud.com>
2023-12-21 14:50:46 +0900

    test: Refactor sorting tests

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -5,24 +5,25 @@ import (
 	"testing"
 )
 
-func TestBubbleSort(t *testing.T) {
-	type args struct {
-		arr []int
-	}
+type args struct {
+	arr []int
+}
 
-	tests := []struct {
-		name string
-		args args
-		want []int
-	}{
-		{"test1", args{[]int{3, 2, 1}}, []int{1, 2, 3}},
-		{"test2", args{[]int{3, 2, 1, 4, 5, 6, 7, 8, 9, 10}}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}},
-		{"test3", args{[]int{3, 7, 2, 8, 1, 6, 4, 0, -5, 6, 9}}, []int{-5, 0, 1, 2, 3, 4, 6, 6, 7, 8, 9}},
-		{"test4", args{[]int{2, 1}}, []int{1, 2}},
-		{"test5", args{[]int{1}}, []int{1}},
-		{"test6", args{[]int{1, 1, 1, 1, 1}}, []int{1, 1, 1, 1, 1}},
-		{"test7", args{[]int{1, 1, 1, 1}}, []int{1, 1, 1, 1}},
-	}
+var tests = []struct {
+	name string
+	args args
+	want []int
+}{
+	{"test1", args{[]int{3, 2, 1}}, []int{1, 2, 3}},
+	{"test2", args{[]int{3, 2, 1, 4, 5, 6, 7, 8, 9, 10}}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}},
+	{"test3", args{[]int{3, 7, 2, 8, 1, 6, 4, 0, -5, 6, 9}}, []int{-5, 0, 1, 2, 3, 4, 6, 6, 7, 8, 9}},
+	{"test4", args{[]int{2, 1}}, []int{1, 2}},
+	{"test5", args{[]int{1}}, []int{1}},
+	{"test6", args{[]int{1, 1, 1, 1, 1}}, []int{1, 1, 1, 1, 1}},
+	{"test7", args{[]int{1, 1, 1, 1}}, []int{1, 1, 1, 1}},
+}
+
+func TestBubbleSort(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			BubbleSort(tt.args.arr)
@@ -34,23 +35,6 @@ func TestBubbleSort(t *testing.T) {
 }
 
 func TestMergeSort(t *testing.T) {
-	type args struct {
-		arr []int
-	}
-
-	tests := []struct {
-		name string
-		args args
-		want []int
-	}{
-		{"test1", args{[]int{3, 2, 1}}, []int{1, 2, 3}},
-		{"test2", args{[]int{3, 2, 1, 4, 5, 6, 7, 8, 9, 10}}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}},
-		{"test3", args{[]int{3, 7, 2, 8, 1, 6, 4, 0, -5, 6, 9}}, []int{-5, 0, 1, 2, 3, 4, 6, 6, 7, 8, 9}},
-		{"test4", args{[]int{2, 1}}, []int{1, 2}},
-		{"test5", args{[]int{1}}, []int{1}},
-		{"test6", args{[]int{1, 1, 1, 1, 1}}, []int{1, 1, 1, 1, 1}},
-		{"test7", args{[]int{1, 1, 1, 1}}, []int{1, 1, 1, 1}},
-	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			MergeSort(tt.args.arr)
@@ -62,23 +46,6 @@ func TestMergeSort(t *testing.T) {
 }
 
 func TestHeapSort(t *testing.T) {
-	type args struct {
-		arr []int
-	}
-
-	tests := []struct {
-		name string
-		args args
-		want []int
-	}{
-		{"test1", args{[]int{3, 2, 1}}, []int{1, 2, 3}},
-		{"test2", args{[]int{3, 2, 1, 4, 5, 6, 7, 8, 9, 10}}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}},
-		{"test3", args{[]int{3, 7, 2, 8, 1, 6, 4, 0, -5, 6, 9}}, []int{-5, 0, 1, 2, 3, 4, 6, 6, 7, 8, 9}},
-		{"test4", args{[]int{2, 1}}, []int{1, 2}},
-		{"test5", args{[]int{1}}, []int{1}},
-		{"test6", args{[]int{1, 1, 1, 1, 1}}, []int{1, 1, 1, 1, 1}},
-		{"test7", args{[]int{1, 1, 1, 1}}, []int{1, 1, 1, 1}},
-	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			Heapsort(tt.args.arr)
@@ -90,23 +57,6 @@ func TestHeapSort(t *testing.T) {
 }
 
 func TestQuickSort(t *testing.T) {
-	type args struct {
-		arr []int
-	}
-
-	tests := []struct {
-		name string
-		args args
-		want []int
-	}{
-		{"test1", args{[]int{3, 2, 1}}, []int{1, 2, 3}},
-		{"test2", args{[]int{3, 2, 1, 4, 5, 6, 7, 8, 9, 10}}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}},
-		{"test3", args{[]int{3, 7, 2, 8, 1, 6, 4, 0, -5, 6, 9}}, []int{-5, 0, 1, 2, 3, 4, 6, 6, 7, 8, 9}},
-		{"test4", args{[]int{2, 1}}, []int{1, 2}},
-		{"test5", args{[]int{1}}, []int{1}},
-		{"test6", args{[]int{1, 1, 1, 1, 1}}, []int{1, 1, 1, 1, 1}},
-		{"test7", args{[]int{1, 1, 1, 1}}, []int{1, 1, 1, 1}},
-	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			QuickSort(tt.args.arr)
@@ -118,23 +68,6 @@ func TestQuickSort(t *testing.T) {
 }
 
 func TestSelectionSort(t *testing.T) {
-	type args struct {
-		arr []int
-	}
-
-	tests := []struct {
-		name string
-		args args
-		want []int
-	}{
-		{"test1", args{[]int{3, 2, 1}}, []int{1, 2, 3}},
-		{"test2", args{[]int{3, 2, 1, 4, 5, 6, 7, 8, 9, 10}}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}},
-		{"test3", args{[]int{3, 7, 2, 8, 1, 6, 4, 0, -5, 6, 9}}, []int{-5, 0, 1, 2, 3, 4, 6, 6, 7, 8, 9}},
-		{"test4", args{[]int{2, 1}}, []int{1, 2}},
-		{"test5", args{[]int{1}}, []int{1}},
-		{"test6", args{[]int{1, 1, 1, 1, 1}}, []int{1, 1, 1, 1, 1}},
-		{"test7", args{[]int{1, 1, 1, 1}}, []int{1, 1, 1, 1}},
-	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			SelectionSort(tt.args.arr)
@@ -146,23 +79,6 @@ func TestSelectionSort(t *testing.T) {
 }
 
 func TestBucketSort(t *testing.T) {
-	type args struct {
-		arr []int
-	}
-
-	tests := []struct {
-		name string
-		args args
-		want []int
-	}{
-		{"test1", args{[]int{3, 2, 1}}, []int{1, 2, 3}},
-		{"test2", args{[]int{3, 2, 1, 4, 5, 6, 7, 8, 9, 10}}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}},
-		{"test3", args{[]int{3, 7, 2, 8, 1, 6, 4, 0, -5, 6, 9}}, []int{-5, 0, 1, 2, 3, 4, 6, 6, 7, 8, 9}},
-		{"test4", args{[]int{2, 1}}, []int{1, 2}},
-		{"test5", args{[]int{1}}, []int{1}},
-		{"test6", args{[]int{1, 1, 1, 1, 1}}, []int{1, 1, 1, 1, 1}},
-		{"test7", args{[]int{1, 1, 1, 1}}, []int{1, 1, 1, 1}},
-	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			BucketSort(tt.args.arr)
