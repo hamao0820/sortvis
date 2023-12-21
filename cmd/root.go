8b58d462f084eef9647173dcf6bf2c62fd32a891
浜田　俊祐 <hamashun0820@icloud.com>
2023-12-21 11:17:52 +0900

    feat: Add logo print statement to root command

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"os"
 
+	"github.com/hamao0820/sortvis/util"
 	"github.com/spf13/cobra"
 )
 
@@ -20,12 +21,7 @@ algorithms: bubble, merge, heap
 in interactive mode, you can step forward
 press q or Ctrl+C to quit`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if num <= 0 || num >= 100 {
-			cobra.CheckErr("num must be between 1 and 99")
-		}
-		if duration <= 0 {
-			cobra.CheckErr("duration must be greater than 0")
-		}
+		cmd.Println(util.Logo)
 	},
 }
 
