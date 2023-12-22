0680d5ca41496b5c26293dcb8fa630ec3b49542b
浜田　俊祐 <hamashun0820@icloud.com>
2023-12-23 08:40:06 +0900

    add: Add SortIterator struct and methods

---
+++ sort/sort.go
@@ -0,0 +1,89 @@
+package sort
+
+import (
+	"sync"
+)
+
+type Status int
+
+const (
+	UnInitialized Status = iota
+	Initialized
+	Sorting
+	Done
+)
+
+type SortIterator struct {
+	Arr    []int
+	cStep  chan struct{}
+	cDone  chan struct{}
+	wg     *sync.WaitGroup
+	Status Status
+	sort   func(arr []int, cStep, cDone chan struct{}, wg *sync.WaitGroup)
+}
+
+var Sorters = map[string]*SortIterator{
+	"bubble": NewSorter(BubbleSortAsync),
+}
+
+func NewSorter(sort func(arr []int, cStep, cDone chan struct{}, wg *sync.WaitGroup)) *SortIterator {
+	return &SortIterator{
+		Arr:    []int{},
+		cStep:  make(chan struct{}),
+		cDone:  make(chan struct{}),
+		wg:     &sync.WaitGroup{},
+		Status: UnInitialized,
+		sort:   sort,
+	}
+}
+
+func (s *SortIterator) Init(arr []int) {
+	s.Arr = arr
+	s.Status = Initialized
+	s.cStep = make(chan struct{})
+	s.cDone = make(chan struct{})
+	s.wg = &sync.WaitGroup{}
+}
+
+func (s *SortIterator) step() {
+	s.cStep <- struct{}{}
+}
+
+func (s *SortIterator) Next() bool {
+	if s.Status == UnInitialized {
+		return false
+	} else if s.Status == Initialized {
+		s.Status = Sorting
+
+		s.wg.Add(1)
+		go s.sort(s.Arr, s.cStep, s.cDone, s.wg)
+		s.wg.Wait()
+
+		return true
+	} else if s.Status == Sorting {
+		select {
+		case <-s.cDone:
+			s.Status = Done
+			close(s.cStep)
+			close(s.cDone)
+			return false
+		default:
+			s.wg.Add(1)
+			s.step()
+			s.wg.Wait()
+
+			select {
+			case <-s.cDone:
+				s.Status = Done
+				close(s.cStep)
+				close(s.cDone)
+				return false
+			default:
+			}
+		}
+
+		return true
+	}
+
+	return false
+}
